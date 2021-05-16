c072527c2ebcab51d2a008daf9311b537f7d3fe3
soumik21 <kundu.soumik008@gmail.com>
2021-05-16 12:09:07 +0530

    driver update

--- src/assignment5.java
+++ src/assignment5.java
@@ -7,7 +7,7 @@ public class assignment5 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\software\\eclipse-java-2020-12-R-win32-x86_64\\eclipse\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 	     WebDriver driver = new ChromeDriver();
 	     driver.manage().window().maximize();
 	     driver.get("https://the-internet.herokuapp.com/nested_frames");
