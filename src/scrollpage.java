cc198ec636e89b55245d81b73bd121a603a05d29
soumik21 <kundu.soumik008@gmail.com>
2021-06-06 11:59:31 +0530

    scroll

---
+++ src/scrollpage.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class scrollpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		 
+		driver.manage().window().maximize();
+		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,500)\r\n");
+		js.executeScript("document.querySelector(\".tableFixHead\").scrollTop=500");
+	}
+
+}
