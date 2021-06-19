4a6ef8b855a7cf6ac8a1cb58aba6403c156015ac
soumik21 <kundu.soumik008@gmail.com>
2021-06-19 23:16:55 +0530

    update

--- src/assignment8.java
+++ src/assignment8.java
@@ -1,13 +1,35 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class assignment8 {
-
-	public static void main(String[] args) {
+	
+	WebDriver driver = null;
+	
+	@Parameters("browsername")
+    @BeforeTest
+    public void setup(String browsername)
+    {
+    	if(browsername.equalsIgnoreCase("chrome"))
+    	{
+    		System.setProperty("webdriver.chrome.driver", "/driver/chromedriver.exe");
+    		WebDriver driver = new ChromeDriver();
+    	}
+    	else if(browsername.equalsIgnoreCase("firefox"))
+    	{
+    		System.setProperty("webdriver.chrome.driver", "/driver/geckodriver.exe");
+    		WebDriver driver = new ChromeDriver();
+    	}
+    }
+   
+	 @Test
+	public void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		//WebDriver driver = new ChromeDriver();
 		 
 		driver.manage().window().maximize();
 		driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
